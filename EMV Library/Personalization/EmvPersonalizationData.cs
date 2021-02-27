8a199a33985c0c7989508d975dd572aa5b45dda2
Tok <github@zetoken.net>
2021-02-27 17:18:39 +0100

    -fix: unused fields removed

--- EMV Library/Personalization/EmvPersonalizationData.cs
+++ EMV Library/Personalization/EmvPersonalizationData.cs
@@ -8,18 +8,6 @@ namespace WSCT.EMV.Personalization
     [DataContract]
     public class EmvPersonalizationData
     {
-        [DataMember]
-        public string Aid { get; set; }
-
-        [DataMember]
-        public string Label { get; set; }
-
-        [DataMember]
-        public string PriorityIndicator { get; set; }
-
-        [DataMember]
-        public IEnumerable<string> LanguagePreference { get; set; }
-
         [JsonExtensionData]
         public IDictionary<string, JToken> UnmanagedAttributes;
     }
